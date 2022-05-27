ee1c3665e8dd38b8543412d2b140f1446011a779
Samat Kalshabekov <skalshabekov@beeline.kz>
2022-05-27 17:26:40 +0600

    first steps to the moon and beyond

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import { ReactComponent as StarWarsLogoSVG } from "./assets/star-wars.svg";
+import { ReactComponent as IconSearchSVG } from "./assets/icon-search.svg";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header
+        style={{
+          display: "grid",
+          gridTemplateColumns: "1fr 1fr 1fr",
+          justifyItems: "center",
+        }}
+      >
+        <p>logo</p>
+        <StarWarsLogoSVG />
+        <div style={{ justifySelf: "flex-end" }}>
+          <div style={{ position: "relative", margin: "1rem" }}>
+            <IconSearchSVG
+              style={{
+                height: "1.5rem",
+                width: "1.5rem",
+                padding: "4px",
+                position: "absolute",
+                boxSizing: "border-box",
+                top: "50%",
+                right: ".5rem",
+                transform: "translateY(-50%)",
+                cursor: "pointer",
+              }}
+            />
+            <input
+              placeholder="Search Star Wars"
+              type="text"
+              style={{
+                boxSizing: "border-box",
+                height: "10px",
+                backgroundColor: "#343434",
+                fontFamily: "DIN, Helvetica, Arial, sans-serif",
+                color: "white",
+                padding: "1rem",
+              }}
+            />
+          </div>
+        </div>
       </header>
     </div>
   );
 }
-
-export default App;
